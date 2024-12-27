43b1372561a9d4de80d3b5ad8e82564657b41815
fer <fernandaavellorocha@gmail.com>
2024-12-27 14:08:34 -0300

    agrego componenets a home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import { FrontMessage } from '../components/Home/FrontMessage';
+import { HomeServices } from '../components/Home/HomeServices';
+import { HomeTeam } from '../components/Home/HomeTeam';
 
 const Home = () => {
   return (
     <div>
       <FrontMessage />
+      <HomeServices />
+      <HomeTeam />
     </div>
   );
 };
