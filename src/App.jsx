b4bb1a7f9bed18c81bbc7fd8f5ad5956e93c1e11
fer <fernandaavellorocha@gmail.com>
2024-12-27 14:08:01 -0300

    Agrego formulario de agendamiento

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import Team from './pages/Team';
 import Services from './pages/Services';
+import Appointment from './pages/Appointment';
 import { ConfigProvider } from 'antd';
 import { theme } from 'antd';
 
@@ -23,10 +25,12 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/team" element={<Team />} />
           <Route path="/" element={<Home />} />
+          <Route path="/team" element={<Team />} />
           <Route path="/services" element={<Services />} />
+          <Route path="/appointment" element={<Appointment />} />
         </Routes>
+        <Footer />
       </Router>
     </ConfigProvider>
   );
